1644e60628d01620cb5f04e44824b0733c6e2041
Shashi Singh <shasingh36@publicisgroupe.net>
2020-05-29 19:01:14 +0530

    Updates

---
+++ precache-manifest.7d11aece518982a6956db57c3c345341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d5f77b44c0f13fc8658ca146f0d3ca",
+    "url": "/rickyandmorty-react/index.html"
+  },
+  {
+    "revision": "6a08a0aebbbf94d74b08",
+    "url": "/rickyandmorty-react/static/css/main.f80badc8.chunk.css"
+  },
+  {
+    "revision": "aee97b2e505deb573a82",
+    "url": "/rickyandmorty-react/static/js/2.1ee2a1a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rickyandmorty-react/static/js/2.1ee2a1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a08a0aebbbf94d74b08",
+    "url": "/rickyandmorty-react/static/js/main.74373045.chunk.js"
+  },
+  {
+    "revision": "fadb4ff6e4ba1ab60479",
+    "url": "/rickyandmorty-react/static/js/runtime-main.a27cc204.js"
+  },
+  {
+    "revision": "e75855a18e3532ccada0d25685656a95",
+    "url": "/rickyandmorty-react/static/media/logout.e75855a1.svg"
+  }
+]);
