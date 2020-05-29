43801886d1c75321dd13913e6387251ce2a30293
Shashi Singh <shasingh36@publicisgroupe.net>
2020-05-29 17:58:23 +0530

    initial code

---
+++ build/precache-manifest.6789a0dfbae5883f9608e44a880c142f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1404b6911e6e7027b0c76340871905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdfac2128d7f543ffa2d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ebddd06d982fc48f65e9",
+    "url": "/static/js/2.29f2ad0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29f2ad0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdfac2128d7f543ffa2d",
+    "url": "/static/js/main.5d1bf17d.chunk.js"
+  },
+  {
+    "revision": "e3caac96b421b5357af2",
+    "url": "/static/js/runtime-main.cefd1b74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
