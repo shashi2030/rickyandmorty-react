079bdeb16083bae349d92b408256d79d0bfc69b2
Shashi Singh <shasingh36@publicisgroupe.net>
2020-05-29 18:33:31 +0530

    Updates

--- precache-manifest.eaf63a8a585612cbe552fc602650a235.js
+++ precache-manifest.eaf63a8a585612cbe552fc602650a235.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9c725c5456e7696c53ddf1b24f7a64d",
+    "revision": "5947e1c87df503bc6e91b5b30848c14e",
     "url": "/rickyandmorty-react/index.html"
   },
   {
-    "revision": "f843c3ee594e3d5ed06b",
+    "revision": "6e527516ac470bb41aa8",
     "url": "/rickyandmorty-react/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "ebddd06d982fc48f65e9",
-    "url": "/rickyandmorty-react/static/js/2.29f2ad0a.chunk.js"
+    "revision": "f099c291a1f0ec107ca3",
+    "url": "/rickyandmorty-react/static/js/2.d2f810eb.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/rickyandmorty-react/static/js/2.29f2ad0a.chunk.js.LICENSE.txt"
+    "url": "/rickyandmorty-react/static/js/2.d2f810eb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f843c3ee594e3d5ed06b",
-    "url": "/rickyandmorty-react/static/js/main.3d901bc4.chunk.js"
+    "revision": "6e527516ac470bb41aa8",
+    "url": "/rickyandmorty-react/static/js/main.0b7d7c9a.chunk.js"
   },
   {
     "revision": "fadb4ff6e4ba1ab60479",
